0fc77bd4a3689c9d4b764399d16bbf0c2a0a468a
IgorDomingos <idrs@cin.ufpe.br>
2019-06-12 12:06:05 -0300

    Modificado(Igor03) - 12/06

--- jogador/CadastroJogadores.java
+++ jogador/CadastroJogadores.java
@@ -1,17 +1,37 @@
 package jogador;
 
 public class CadastroJogadores {
-	private RepositorioJogadores RepositorioJogadores;
+	private RepositorioJogadores repositorioJogadores;
 	
 	public CadastroJogadores (RepositorioJogadores repositorio) {
-		this.RepositorioJogadores = repositorio;
+		this.repositorioJogadores = repositorio;
 	}
 	public void cadastrar(Jogador jogador) throws JogadorJaCadastradoException, JogadorNaoEncontradoException {
-		if (!this.RepositorioJogadores.existe(jogador.getNome())) {
-			this.RepositorioJogadores.inserir(jogador);
+		if (!this.repositorioJogadores.existe(jogador.getNome())) {
+			this.repositorioJogadores.inserir(jogador);
 		} else {
 			throw new JogadorJaCadastradoException();
 		}
 	}
-	
+	public void remover(Jogador jogador) throws JogadorNaoEncontradoException {
+		if (this.repositorioJogadores.existe(jogador.getNome()) == true) {
+			this.repositorioJogadores.remover(jogador.getNome());
+		} else {
+			throw new JogadorNaoEncontradoException();
+		}
+	}
+	public void atualizar(Jogador jogador) throws JogadorNaoEncontradoException {
+		if (this.repositorioJogadores.existe(jogador.getNome()) == true) {
+			this.repositorioJogadores.atualizar(jogador);
+		} else {
+			throw new JogadorNaoEncontradoException();
+		}
+	}
+	public void procurar(Jogador jogador) throws JogadorNaoEncontradoException{
+		if (this.repositorioJogadores.existe(jogador.getNome()) == true) {
+			this.repositorioJogadores.procurar(jogador.getNome());
+		} else {
+			throw new JogadorNaoEncontradoException();
+		}
+	}
 }
