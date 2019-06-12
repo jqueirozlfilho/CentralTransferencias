89de73ccef3058c4ebbaf895d905b9f3c2a3c2ce
mlas2 <mlas2@cin.ufpe.br>
2019-06-12 10:36:26 -0300

    pacote Time

---
+++ malu/time/Time.java
@@ -0,0 +1,40 @@
+package time;
+
+import pessoa.Pessoa;
+import jogador.RepositorioJogadores;
+import treinador.Treinador;
+
+public class Time {
+	private String nome;
+	private double verbaTotal;
+	private RepositorioJogadores jogadores;
+	private Treinador treinador;
+	
+	public Time () {
+		this.nome = null;
+		this.verbaTotal = 0.00;
+		this.jogadores = null;
+		this.treinador = null;
+	}
+	
+	public String getNome () {
+		return nome;
+	}
+	
+	public double getVerba () {
+		return verbaTotal;
+	}
+	
+	public RepositorioJogadores getJogadores () {
+		return this.jogadores;
+	}
+	
+	public void setNome (String nome) {
+		this.nome = nome;
+	}
+	
+	public void setVerba (double verbaAtual) {
+		this.verbaTotal = verbaAtual;
+	}
+
+}
