dddf0bf5258e829198e080c34c36cb40c96091f9
Henrique1701 <48698947+Henrique1701@users.noreply.github.com>
2019-06-15 16:39:29 -0300

    Atualização 15/06

--- habilidade/Habilidade.java
+++ habilidade/Habilidade.java
@@ -1,49 +1,59 @@
 package habilidade;
 
 public class Habilidade {
+		
+	private String nomeJogador;
+	private double notaTotal;
+	private int finalizacao;
+	private int passe;
+	private int interceptacao;
 	
-		private double notaTotal;
-		private int finalizacao;
-		private int passe;
-		private int interceptacao;
-		
-		public Habilidade() {			
+	public Habilidade() {			
+		this.nomeJogador = "";
+		this.notaTotal = 0;
+		this.finalizacao = 0;
+		this.passe = 0;
+		this.interceptacao = 0;
+	}
+	
+	
+	public int getFinalizacao() {
+		return this.finalizacao;
+	}
+	
+	public void setFinalizacao(int finalizacao) {
+		this.finalizacao=finalizacao;
+	}
+	
+	public int getInterceptacao() {
+		return this.interceptacao;
+	}
+	
+	public void setInterceptacao (int interceptacao) {
+		this.interceptacao = interceptacao;
+	}
+	
+	public int getPasse() {
+		return this.passe;
+	}
 
-			this.finalizacao = 0;
-			this.passe = 0;
-			this.interceptacao = 0;
-		}
+	public void setPasse(int passe) {
+		this.passe = passe;
+	}	
+	
+	public double getNotaTotal() {
 		
+		this.notaTotal=((this.interceptacao + this.passe+ this.finalizacao)/3);
 		
-		public int getfinalizacao() {
-	        return finalizacao;
-	    }
-		
-		public void setfinalizacao(int finalizacao) {
-	        this.finalizacao=finalizacao;
-	    }
-		
-		public int getinterceptacao() {
-			return this.interceptacao;
-		}
-		
-		public void setinterceptacao (int interceptacao) {
-			this.interceptacao = interceptacao;
-		}
-		
-		public int getpasse() {
-			return passe;
-		}
+		return this.notaTotal;
+	
+	}
 
-	    public void setpasse(int passe) {
-	        this.passe = passe;
-	    }	
-		
-		public double notaTotal(int interceptacao, int passe, int finalizacao) {
-			
-			notaTotal=((interceptacao+passe+finalizacao)/3);
-			
-			return notaTotal;
-		
+	public String getNomeJogador() {
+		return this.nomeJogador;
+	}
+
+	public void setNomeJogador(String nomeJogador) {
+		this.nomeJogador = nomeJogador;
 	}
 }
